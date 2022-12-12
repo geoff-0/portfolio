9a5eafd6e1bd05f7243a9f92b4e4381ac3145fed
Geoffrey Perez <geoday@DESKTOP-EPUK3OS.localdomain>
2022-12-11 22:15:01 -0800

    first commit

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: "Geoffrey Perez: A Front-end Web Developer",
+  titleTemplate: "%s",
+  defaultTitle: "Geoffrey Perez: A Front-end Web Developer",
+  description: "Programmer specializing in front-end development.",
+  canonical: "https://geoday.dev",
+  openGraph: {
+    url: "https://geoday.dev",
+    title: "Geoffrey Perez: A Front-end Web Developer",
+    description: "Programmer specializing in front-end development.",
+    site_name: "Geoffrey Perez: A Front-end Web Developer",
+  },
+};
+
+export default defaultSEOConfig;
