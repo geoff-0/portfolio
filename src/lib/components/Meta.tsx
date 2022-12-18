2a52aea4a562815f6e362f3f22fac641499eef66
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-17 22:40:49 -0800

    seo embed brand color

--- src/lib/components/Meta.tsx
+++ src/lib/components/Meta.tsx
@@ -9,7 +9,7 @@ const Meta = () => {
       <meta name="apple-mobile-web-app-title" content={APP_NAME} />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="theme-color" content="#FFFFFF" />
+      <meta name="theme-color" content="brand.primary" />
 
       <link rel="shortcut icon" href="/favicon.svg" />
 
