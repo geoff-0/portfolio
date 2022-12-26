79a24887d4d85f45f4a1716ce6fd7f648f916b8e
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-25 21:33:14 -0800

    app manifest

--- src/lib/components/Meta.tsx
+++ src/lib/components/Meta.tsx
@@ -11,10 +11,6 @@ const Meta = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#1A9FFF" />
 
-      <meta property="og:image" content="/portfolio-snapshot.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="600" />
-
       <link rel="shortcut icon" href="/favicon.svg" />
 
       <link rel="manifest" href="/manifest.json" />
