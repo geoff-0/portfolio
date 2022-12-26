8fc9a67b6be56f5cba0b3f7b0ce0bfab479990ff
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-25 21:13:00 -0800

    seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -5,14 +5,6 @@ const defaultSEOConfig = {
   defaultTitle: "Geoffrey Perez",
   description: "Self-taught programmer specializing in front-end development",
   canonical: "https://geoday.dev",
-  openGraph: {
-    url: "https://geoday.dev",
-    type: "object",
-    images: [{ url: "/portfolio-snapshot.png", width: 1200, height: 600 }],
-    title: "Geoffrey Perez",
-
-    description: "Self-taught programmer specializing in front-end development",
-  },
 };
 
 export default defaultSEOConfig;
