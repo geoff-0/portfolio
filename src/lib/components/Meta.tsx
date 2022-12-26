75cdd986cb7dbe87afd74934438e3d8c44bd7242
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-25 21:19:38 -0800

    seo?

--- src/lib/components/Meta.tsx
+++ src/lib/components/Meta.tsx
@@ -11,6 +11,7 @@ const Meta = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#1A9FFF" />
 
+      <meta property="og:image" content="/portfolio-snapshot.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="600" />
 
