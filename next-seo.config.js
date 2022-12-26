51e5ef85935b7789dfb0d4f93051c0c21fc63c8f
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-25 20:02:10 -0800

    seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "Geoffrey Perez: Front-end Developer",
+  title: "Geoffrey Perez | Front-end Developer",
   titleTemplate: "%s",
-  defaultTitle: "Geoffrey Perez: Front-end Developer",
   description: "Programmer specializing in front-end development.",
   canonical: "https://geoday.dev",
   openGraph: {
     url: "https://geoday.dev",
-    title: "Geoffrey Perez: A Front-end Developer",
+    title: "Geoffrey Perez | A Front-end Developer",
     description: "Programmer specializing in front-end development.",
-    site_name: "Geoffrey Perez: Front-end Developer",
+    site_name: "Geoffrey Perez | Front-end Developer",
   },
 };
 
