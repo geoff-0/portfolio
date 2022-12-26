41f91bdff06dfacb95808e753c7cac8381b4ae19
Geoffrey Perez <geoday.ai.si@gmail.com>
2022-12-25 20:33:49 -0800

    seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,16 +3,12 @@ const defaultSEOConfig = {
   title: "Geoffrey Perez | Front-end Developer",
   titleTemplate: "%s",
   defaultTitle: "Geoffrey Perez | Front-end Developer",
-  description: "Programmer specializing in front-end development.",
+  description: "Self-taught programmer specializing in front-end development",
   canonical: "https://geoday.dev",
   openGraph: {
     url: "https://geoday.dev",
     title: "Geoffrey Perez | A Front-end Developer",
-    defaultImageHeight: 495,
-    defaultImageWidth: 943,
-    images: [{ url: "/portfolio-snapshot.png", alt: "Portfolio Snapshot" }],
-    description: "Programmer specializing in front-end development.",
-    site_name: "Geoffrey Perez | Front-end Developer",
+    description: "Self-taught programmer specializing in front-end development",
   },
 };
 
--- src/lib/components/Meta.tsx
+++ src/lib/components/Meta.tsx
@@ -11,6 +11,8 @@ const Meta = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#1A9FFF" />
 
+      <meta property="og:image" content="/portfolio-snapshot.png" />
+
       <link rel="shortcut icon" href="/favicon.svg" />
 
       <link rel="manifest" href="/manifest.json" />
